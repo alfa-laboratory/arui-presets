47a0ca9f680e9ca64767851a4f90e1a4d0f0937c
akitov <akitov@alfabank.ru>
2017-03-10 12:38:30 +0300

    feat(*): up all dependencies, fix eslint rules

--- eslint/index.js
+++ eslint/index.js
@@ -10,7 +10,9 @@ module.exports = {
         }
     },
     env: {
-        mocha: true
+        browser: true,
+        mocha: true,
+        node: true
     },
     globals: {
         benchmark: true,
@@ -21,35 +23,52 @@ module.exports = {
         suite: true
     },
     plugins: [
+        'chai-friendly',
+        'class-methods-use-this-regexp',
         'jsdoc'
     ],
     rules: {
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
-        'prefer-const': 0,
-        'comma-dangle': ['error', 'never'],
-        'func-names': 0,
-        'default-case': 0,
-        'prefer-template': 0,
-        'no-param-reassign': 0,
-        'no-nested-ternary': 0,
-        'prefer-rest-params': 0,
-        'no-unused-expressions': 0,
-        'no-extend-native': 0,
-        'jsx-quotes': ['error', 'prefer-single'],
-        'no-shadow': 0,
-        'max-len': ['error', 120, 4, { 'ignoreComments': true }],
-        'prefer-arrow-callback': 0,
-        'react/prefer-stateless-function': 0,
-        'react/react-in-jsx-scope': 0,
-        'react/jsx-indent-props': [2, 4],
-        'react/jsx-boolean-value': ['error', 'always'],
-        'valid-jsdoc': ['error', {
-            'requireReturn': false,
-            'requireReturnDescription': false,
-            'preferType': { 'boolean': 'Boolean', 'number': 'Number', 'object': 'Object', 'string': 'String' },
-            'matchDescription': '(.+\\.)|^$',
-            'prefer': { 'return': 'returns' }
+        'class-methods-use-this': 0,
+        'class-methods-use-this-regexp/class-methods-use-this': [2, {
+            'exceptMethods': [
+                'blur',
+                'render*',
+                'getInitialState',
+                'getDefaultProps',
+                'componentWillMount',
+                'componentDidMount',
+                'componentWillReceiveProps',
+                'shouldComponentUpdate',
+                'componentWillUpdate',
+                'componentDidUpdate',
+                'componentWillUnmount'
+            ]
         }],
-        'jsdoc/newline-after-description': 'error'
+        'chai-friendly/no-unused-expressions': 2,
+        'comma-dangle': [2, 'never'],
+        'default-case': 0,
+        'indent': [2, 4, { 'SwitchCase': 1 }],
+        'jsdoc/newline-after-description': 2,
+        'jsx-quotes': [2, 'prefer-single'],
+        'max-len': [2, 120, 4, { 'ignoreComments': true }],
+        'no-param-reassign': 0,
+        'no-plusplus': [2, { 'allowForLoopAfterthoughts': true }],
+        'no-shadow': 0,
+        'no-unused-expressions': 0,
+        'prefer-arrow-callback': 0,
+        'prefer-const': 0,
+        'react/jsx-boolean-value': [2, 'always'],
+        'react/jsx-curly-spacing': [2, 'always'],
+        'react/jsx-filename-extension': [2, { 'extensions': ['.jsx'] }],
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        'react/prefer-stateless-function': 0,
+        'valid-jsdoc': [2, {
+            'matchDescription': '(.+\\.)|^$',
+            'prefer': { 'return': 'returns' },
+            'preferType': { 'boolean': 'Boolean', 'number': 'Number', 'object': 'Object', 'string': 'String' },
+            'requireReturn': false,
+            'requireReturnDescription': false
+        }]
     }
 };
