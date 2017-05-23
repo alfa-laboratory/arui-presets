1d9fe55c0d68ffb48b015eb24c31c50b5126885b
Alexandr Kitov <akitov@alfabank.ru>
2017-05-23 12:12:20 +0300

    fix(babel): babel-runtime isn't a plugin

--- babel.js
+++ babel.js
@@ -13,8 +13,7 @@ module.exports = {
             plugins: [
                 require('babel-plugin-transform-react-remove-prop-types'),
                 require('babel-plugin-transform-react-constant-elements'),
-                require('babel-plugin-transform-react-inline-elements'),
-                require('babel-runtime')
+                require('babel-plugin-transform-react-inline-elements')
             ]
         }
     }
