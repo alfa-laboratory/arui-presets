90376f669745c47509eb895aa3eb49da76821e2e
Волков Даниил Анатольевич <DVolkov2@alfabank.ru>
2017-10-16 17:43:17 +0300

    fix(*): fix plugins for env presets

--- babel.js
+++ babel.js
@@ -14,6 +14,7 @@ module.exports = {
     plugins: [
         'babel-plugin-transform-decorators-legacy',
         'babel-plugin-transform-class-properties',
+        'babel-plugin-transform-export-extensions',
         ['babel-plugin-transform-object-rest-spread', { useBuiltIns: true }],
         ['babel-plugin-transform-runtime', { polyfill: false, helpers: false }]
     ],
