e75a4cb19090976f2e7041182368c9d8988e5ca2
stepancar <stepancar@hotmail.com>
2017-10-30 01:09:29 +0300

    fix(*): fix ie10 support

--- test/babel-test.js
+++ test/babel-test.js
@@ -11,24 +11,3 @@ const browserCode = babel.transform(jsFile, {
 
 console.log('browser code:\n', browserCode); // eslint-disable-line no-console
 
-// babel with es2015 modules
-const browserCodeModules = babel.transform(jsFile, {
-    presets: [[require.resolve('../babel.js'), { modules: false }]]
-}).code;
-
-console.log('browser code without modules:\n', browserCodeModules); // eslint-disable-line no-console
-
-// babel-node with default module
-const nodeCode = babel.transform(jsFile, {
-    presets: [require.resolve('../babel-node.js')]
-}).code;
-
-console.log('node code:\n', nodeCode); // eslint-disable-line no-console
-
-
-// babel with es2015 modules
-const nodeModulesCode = babel.transform(jsFile, {
-    presets: [[require.resolve('../babel-node.js'), { modules: false }]]
-}).code;
-
-console.log('node code without modules:\n', nodeModulesCode); // eslint-disable-line no-console
