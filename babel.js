69c0c77bbf27a56f0bde0a3147f41e1df227316c
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-10-30 19:40:56 +0300

    feat(babel): add syntax-dynamic-import plugin

--- babel.js
+++ babel.js
@@ -14,19 +14,20 @@ module.exports = {
         'react'
     ],
     plugins: [
-        'babel-plugin-transform-proto-to-assign',
-        'babel-plugin-transform-decorators-legacy',
-        'babel-plugin-transform-class-properties',
-        'babel-plugin-transform-export-extensions',
-        ['babel-plugin-transform-object-rest-spread', { useBuiltIns: true }],
-        ['babel-plugin-transform-runtime', { polyfill: false, helpers: false }]
+        'syntax-dynamic-import',
+        'transform-proto-to-assign',
+        'transform-decorators-legacy',
+        'transform-class-properties',
+        'transform-export-extensions',
+        ['transform-object-rest-spread', { useBuiltIns: true }],
+        ['transform-runtime', { polyfill: false, helpers: false }]
     ],
     env: {
         production: {
             plugins: [
-                'babel-plugin-transform-react-remove-prop-types',
-                'babel-plugin-transform-react-constant-elements',
-                'babel-plugin-transform-react-inline-elements'
+                'transform-react-remove-prop-types',
+                'transform-react-constant-elements',
+                'transform-react-inline-elements'
             ]
         }
     }
